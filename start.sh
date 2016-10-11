3aac68653c3f110fa79d128ba16417cac82b1253
Tamara Temple <tamouse@gmail.com>
2016-10-10 22:05:06 -0500

    yolo

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tmux new -s live_jekyll -d
-tmux send-keys -t live_jekyll 'bundle exec guard' C-m
-tmux split-window -v -t live_jekyll
-tmux send-keys -t live_jekyll 'bundle exec jekyll serve' C-m
-tmux attach -t live_jekyll
